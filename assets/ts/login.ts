03252c9bde795ebc4f8a28179654778ec54ddc9d
pierocarrion <piero.january15@gmail.com>
2022-03-26 15:27:02 -0500

    Fix Login User

--- assets/ts/login.ts
+++ assets/ts/login.ts
@@ -5,11 +5,7 @@ export default function(dbUser:User, formInfo:{username:string, password:string}
     if(formInfo.username === dbUser.username && formInfo.password === dbUser.password){
         localStorage.setItem('username',formInfo.username);
         //localStorage.setItem('password',formInfo.password);
-        document.getElementById('perfilUsuario')!.innerHTML = `<img src="${dbUser.enlaceImagen}" alt=""></img>
-                                                                <p>
-                                                                    <br>
-                                                                    ${dbUser.descripcion}
-                                                                </p>`;
+        
 
         loggedIn();
     } else {
